d614c53cb45a567229304fa59f7adf12ad7a302f
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-04-10 13:46:18 +0200

    fix: signal C

--- server/main.c
+++ server/main.c
@@ -1,20 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h> // Ajout de la bibliothèque signal.h
 #include <mariadb/mysql.h>
 #include <bcrypt.h>
 #include "includes/liblinux.h"
 #include <unistd.h>
-#include<sys/types.h> 
-#include<sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #define clear() printf("\033[H\033[J")
 
 #define POLL_INTERVAL_SECONDS 10
+
+void sigint_handler(int signum);
 
 int main(int argc, char const *argv[])
 {
     MYSQL *conn;
     int num_files;
     FileInfo *files;
+
+    signal(SIGINT, sigint_handler);
+
     init_shell();
     conn = mysql_init(NULL);
     if (conn == NULL)
@@ -45,4 +52,21 @@ int main(int argc, char const *argv[])
 
     check_file_changes(conn);
     return 0;
+}
+
+void sigint_handler(int signum)
+{
+    char c;
+
+    signal(signum, SIG_IGN);
+    printf("\nOUCH, did you hit Ctrl-C ?\n"
+           "Do you really want to quit ? [y/n] ");
+    c = getchar();
+    if (c == 'y' || c == 'Y') {
+        printf("Stopping service... GoodBye!");
+        exit(0);
+    } else {
+        signal(SIGINT, sigint_handler);
+    }    
+    getchar();
 }
--- test/test.c
+++ test/test.c
@@ -1,78 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <mariadb/mysql.h>
-#include <bcrypt.h>
-
-// Structure pour stocker les données utilisateur
-struct User {
-    char username[100];
-    char password_hash[BCRYPT_HASHSIZE];
-};
-
-// Fonction pour comparer les mots de passe hachés
-int compare_passwords(const char *provided_password, const char *stored_hash) {
-    if (bcrypt_checkpw(provided_password, stored_hash) == 0) {
-        // Le mot de passe fourni correspond au hachage stocké
-        return 1;
-    } else {
-        // Le mot de passe fourni ne correspond pas au hachage stocké
-        return 0;
-    }
-}
+#include <stdlib.h>
+#include <limits.h>
 
 int main() {
-    MYSQL *conn;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
+    char resolved_path[PATH_MAX];
 
-    conn = mysql_init(NULL);
+    // Chemin absolu du fichier
+    const char *absolute_path = "s.txt";
 
-    if (conn == NULL) {
-        fprintf(stderr, "Erreur lors de l'initialisation de la connexion à la base de données.\n");
-        return 1;
+    // Obtention du chemin d'accès absolu
+    if (realpath(absolute_path, resolved_path) == NULL) {
+        perror("Erreur lors de la résolution du chemin d'accès absolu");
+        return EXIT_FAILURE;
     }
 
-    if (mysql_real_connect(conn, "localhost", "admin", "2002", "nemesisbdd", 0, NULL, 0) == NULL) {
-        fprintf(stderr, "Erreur lors de la connexion à la base de données : %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return 1;
-    }
+    printf("Chemin d'accès absolu : %s\n", resolved_path);
 
-    if (mysql_query(conn, "SELECT username, password FROM users WHERE username='root'")) {
-        fprintf(stderr, "Erreur lors de l'exécution de la requête : %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return 1;
-    }
-
-    res = mysql_use_result(conn);
-    if (res == NULL) {
-        fprintf(stderr, "Erreur lors de la récupération du résultat de la requête.\n");
-        mysql_close(conn);
-        return 1;
-    }
-
-    struct User user;
-
-    while ((row = mysql_fetch_row(res)) != NULL) {
-        strcpy(user.username, row[0]);
-        strcpy(user.password_hash, row[1]);
-    }
-
-    mysql_free_result(res);
-
-    // Demander le mot de passe à l'utilisateur
-    char password[100];
-    printf("Entrez votre mot de passe : ");
-    scanf("%s", password);
-
-    // Comparer les mots de passe hachés
-    if (compare_passwords(password, user.password_hash)) {
-        printf("Le mot de passe est correct.\n");
-    } else {
-        printf("Le mot de passe est incorrect.\n");
-    }
-
-    mysql_close(conn);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
