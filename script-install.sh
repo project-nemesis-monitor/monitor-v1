0bb5a4983fde6795826f0f51a1dd2eecf92c65d3
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2024-04-11 15:19:11 +0200

    fix: shell script

--- script-install.sh
+++ script-install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y build-essential libmariadb-dev mariadb-server
+packages=("build-essential" "libmariadb-dev" "mysql-server")
+missing_packages=()
+
+for pkg in "${packages[@]}"; do
+    if ! dpkg -l | grep -q $pkg; then
+        missing_packages+=("$pkg")
+    fi
+done
+
+if [ ${#missing_packages[@]} -gt 0 ]; then
+    sudo apt-get update
+    sudo apt-get install -y ${missing_packages[@]}
+fi
 
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS nemesisbdd;"
 sudo mysql -e "CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY '2002';"
@@ -10,12 +21,12 @@ sudo mysql -e "FLUSH PRIVILEGES;"
 
 sudo mysql nemesisbdd < nemesisbdd.sql
 
-gcc -Wall server/main.c server/includes/liblinux.c server/includes/reactwthbdd.c -o ./output/server $(mariadb_config --cflags --libs)
+gcc -Wall server/main.c server/includes/liblinux.c server/includes/reactwthbdd.c -o ./output/server $(mysql_config --cflags --libs)
 
 service_file="/etc/systemd/system/monitor-nemesis.service"
 service_content="[Unit]
 Description=Monitor Nemesis Service
-After=network.target mariadb.service
+After=network.target mysql.service
 
 [Service]
 Type=simple
